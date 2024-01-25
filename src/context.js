e09ba7556ce47d698bfa420c9a465649ddb0daae
ENNA-Egor <e.yudakov@gmail.com>
2024-01-25 08:55:20 +0300

    Начало подготовки редюсера

--- src/context.js
+++ src/context.js
@@ -1,13 +1,33 @@
-import { createContext} from 'react';
+import { createContext, useReducer} from 'react';
+import {reduser} from './reduser';
 
 
 export const ShopContext = createContext();
 
-const value = {
-    example: 'hello from context',
-    thankyou: 'Спасибо за покупку'
+const intialState = {
+    goods: [],
+    loading : true,
+    order : [],
+    isBasketShow : false,
+    alertName : '',
 }
 
+// const value = {
+//     example: 'hello from context',
+//     thankyou: 'Спасибо за покупку'
+// }
+
 export const ContextProvider = ({children}) => {
+    const [value, dispatch] = useReducer (reduser, intialState)
+
+    value.closeAlert =()=> {
+        dispatch({type: 'CLOSE_ALERT'})
+    }
+
+    value.removeFromBasket = (itemId) => {
+        dispatch({type: 'REMOVE_FROM_BASKET', payload: {id: itemId}})
+    }
+
+
     return <ShopContext.Provider value={value}>{children}</ShopContext.Provider>
 }
